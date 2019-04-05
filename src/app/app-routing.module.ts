82ff40126945afa6697746b91ead1758c4efa9e3
Elnois Norat <elnois.norat@gmail.com>
2019-04-04 22:13:18 -0500

    Menu Layout, Profile Menu and other updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './auth/register/register.module#RegisterPageModule' },
+  { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' },
+  { path: 'main', loadChildren: './autmenu/main/main.module#MainPageModule' },
 ];
 
 @NgModule({
