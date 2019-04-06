f39015f1590fb2ceb652650e666618796e435463
RourkeGoat <eduardo.rivera8@upr.edu>
2019-04-06 16:15:10 -0400

    ConnectionTest and Queries

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, jsonify
 ##from handler.worker import WorkerHandler
 import stripe
+import time
+import datetime
 ##from config.validation import isWorker
 app = Flask(__name__)
 sKey  = 'sk_test_qKdVTRj6NXM8EeLLUYnzXISS00K3MLJqu3'
@@ -34,9 +36,28 @@ def pay():
     try:
         print("Getting Outcome...")
         customer = stripe.Customer.create(email=request.form['stripeEmail'], source=request.form['stripeToken'])
-        charge = stripe.Charge.create(customer=customer.id, amount=990, currency='usd', description='Bike')
+        #charge = stripe.Charge.create(customer=customer.id, amount=990, currency='usd', description='Bike')
+        subscription = stripe.Subscription.create(
+            customer=customer['id'],
+            items=[{'plan': '2'}],
+        )
         print("Getting Outcome...")
-        print("This is the charge: " + charge['outcome'])
+        #print("This is the charge: " + charge['outcome'])
+        print(subscription)
+        dt = datetime.datetime(2019, 4, 19)
+        print(int(time.mktime(dt.timetuple())))
+        stripe.Subscription.modify(
+            subscription['id'],
+            #quantity = 1,
+            trial_end = int(time.mktime(dt.timetuple())),
+            #prorate=False
+            cancel_at_period_end=False,
+            items=[{
+                'id': subscription['items']['data'][0].id,
+                'plan': '3',
+            }]
+
+        )
         pass
     except stripe.error.CardError as e:
         # Since it's a decline, stripe.error.CardError will be caught
@@ -52,38 +73,44 @@ def pay():
     except stripe.error.RateLimitError as e:
         # Too many requests made to the API too quickly
         print('RateLimitError')
-        print(charge['outcome'])
+        #print(charge['outcome'])
         pass
     except stripe.error.InvalidRequestError as e:
         # Invalid parameters were supplied to Stripe's API
         print('InvalidRequestError')
-        print(charge['outcome'])
+        print(e)
         pass
     except stripe.error.AuthenticationError as e:
         # Authentication with Stripe's API failed
         # (maybe you changed API keys recently)
         print('AuthenticationError')
-        print(charge['outcome'])
+        #print(charge['outcome'])
         pass
     except stripe.error.APIConnectionError as e:
         # Network communication with Stripe failed
         print('APIConnectionError')
-        print(charge['outcome'])
+        #print(charge['outcome'])
         pass
     except stripe.error.StripeError as e:
         # Display a very generic error to the user, and maybe send
         # yourself an email
         print('StripeError')
-        print(charge['outcome'])
+       #print(charge['outcome'])
         pass
     except Exception as e:
         # Something else happened, completely unrelated to Stripe
         print('Exception')
-        print(charge['outcome'])
+        #print(charge['outcome'])
         pass
 
     return "Done"
 
+
+@app.route('/update', methods =['POST'])
+def update():
+
+
+    return "Done"
 
 
 @app.route('/workerLogin')
