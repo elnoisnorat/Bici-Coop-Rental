32369a360cddc8921dbbf9bc93dbb1b3973a09cf
RourkeGoat <eduardo.rivera8@upr.edu>
2019-04-07 16:30:03 -0400

    Delete dbconfig.py

--- config/dbconfig.py
+++
@@ -1,7 +0,0 @@
-pg_config = {
-    'dbname' : 'BiciProto',
-    'user' : 'rourkegoat',
-    'passwd' : 'edispace1',
-    'host' : 'protobici.ctbimbd9lrqx.us-east-2.rds.amazonaws.com',
-    'port' : '5432'
-}
