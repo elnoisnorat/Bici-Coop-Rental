861acbb5dbbc0a6eb7e725b182ac3aef65ef9cb1
RourkeGoat <eduardo.rivera8@upr.edu>
2019-04-16 11:31:56 -0400

    Add files via upload

--- RouteQueries.sql
+++ RouteQueries.sql
@@ -2,38 +2,45 @@
 Create Table Statements
 
  */
-CREATE TABLE Users(UID SERIAL PRIMARY KEY, FName VARCHAR(50), LName VARCHAR(50), password TEXT, PNumber smallint, Email VARCHAR(100) UNIQUE);
-CREATE TABLE Client(CID SERIAL PRIMARY KEY , UID INTEGER REFERENCES Users(UID));
-CREATE TABLE Worker(WID SERIAL PRIMARY KEY, UID INTEGER REFERENCES Users(UID), Status VARCHAR(15));
-CREATE TABLE Admin(AID SERIAL PRIMARY KEY, UID INTEGER REFERENCES Users(UID));
-
-CREATE TABLE Bike(BID Serial PRIMARY KEY, LP VARCHAR(15), RFID VARCHAR(50), Status varchar(15), brand TEXT, model TEXT);
-CREATE TABLE RentLink(RID INTEGER REFERENCES Rental(RID), TID INTEGER REFERENCES Transactions(TID), PRIMARY KEY(TID, RID));
-CREATE TABLE Rental(RID serial PRIMARY KEY, STime TIMESTAMP, ETime TIMESTAMP, Client INTEGER REFERENCES Client (CID), BID INTEGER REFERENCES Bike(BID), DispatchedBy INTEGER REFERENCES Worker(WID), RecivedBy INTEGER REFERENCES Worker(WID), dueDate TIMESTAMP);
-CREATE TABLE Transactions(TID SERIAL PRIMARY KEY, stamp timestamp, Token varchar(50), CID INTEGER REFERENCES Client(CID), Status VARCHAR(15));
-CREATE TABLE Maintenance(MID SERIAL PRIMARY KEY, StartTime TIMESTAMP, EndTime TIMESTAMP, Status varchar(15), Notes VARCHAR(180), BID INTEGER REFERENCES Bike(BID), RequestedBy INTEGER REFERENCES Worker(WID), ServicedBy INTEGER REFERENCES Worker(WID));
-
-CREATE TABLE DecommissionReq( DQID SERIAL PRIMARY KEY , RequestedBy INTEGER REFERENCES Users(UID), AnsweredBy INTEGER REFERENCES Admin(AID), Status VARCHAR(15),  BID INTEGER REFERENCES Bike(BID));
-CREATE TABLE ServiceMaintenance(SMID SERIAL PRIMARY KEY, FName varchar(50), LName varchar(50), Bike varchar(50), Service varchar(100), Price REAL, WorkedBy INTEGER REFERENCES Worker(WID), WorkStatus varchar(15), Notes VARCHAR(180), STime TIMESTAMP, ETime TIMESTAMP);
-CREATE TABLE PunchCard(PCID serial primary key, stampType varchar(5), stamp timestamp );
+CREATE TABLE IF NOT EXISTS Users(UID SERIAL PRIMARY KEY, FName VARCHAR(50), LName VARCHAR(50), password TEXT, PNumber INTEGER, Email VARCHAR(100) UNIQUE, Confirmation BOOLEAN, LogAttempt INTEGER, Blocked TIMESTAMP);
+CREATE TABLE IF NOT EXISTS Client(CID SERIAL PRIMARY KEY , UID INTEGER REFERENCES Users(UID) CHECK(NOT NULL), DebtorFlag BOOLEAN);
+CREATE TABLE IF NOT EXISTS Worker(WID SERIAL PRIMARY KEY, UID INTEGER REFERENCES Users(UID) CHECK(NOT NULL), Status VARCHAR(15));
+CREATE TABLE IF NOT EXISTS Admin(AID SERIAL PRIMARY KEY, UID INTEGER REFERENCES Users(UID) CHECK(NOT NULL));
+CREATE TABLE IF NOT EXISTS Bike(BID Serial PRIMARY KEY, LP VARCHAR(15), RFID VARCHAR(50) UNIQUE, Status varchar(15), brand TEXT, model TEXT);
+CREATE TABLE IF NOT EXISTS Rental(RID serial PRIMARY KEY, STime TIMESTAMP, ETime TIMESTAMP, Client INTEGER REFERENCES Client (CID), BID INTEGER REFERENCES Bike(BID), DispatchedBy INTEGER REFERENCES Worker(WID), ReceivedBy  INTEGER REFERENCES Worker(WID), dueDate TIMESTAMP);
+CREATE TABLE IF NOT EXISTS Transactions(TID SERIAL PRIMARY KEY, stamp timestamp, Token varchar(50), CID INTEGER REFERENCES Client(CID) CHECK(NOT NULL), Status VARCHAR(15), Cost REAL, SMID INTEGER REFERENCES ServiceMaintenance(SMID));
+CREATE TABLE IF NOT EXISTS RentLink(RID INTEGER REFERENCES Rental(RID), TID INTEGER REFERENCES Transactions(TID), PRIMARY KEY(TID, RID));
+CREATE TABLE IF NOT EXISTS Maintenance(MID SERIAL PRIMARY KEY, StartTime TIMESTAMP, EndTime TIMESTAMP, Status varchar(15), Notes VARCHAR(180), BID INTEGER REFERENCES Bike(BID), RequestedBy INTEGER REFERENCES Users(UID), ServicedBy INTEGER REFERENCES Worker(WID), Service text);
+CREATE TABLE IF NOT EXISTS DecommissionReq( DQID SERIAL PRIMARY KEY , RequestedBy INTEGER REFERENCES Users(UID) CHECK(NOT NULL), AnsweredBy INTEGER REFERENCES Admin(AID), Status VARCHAR(15),  BID INTEGER REFERENCES Bike(BID) CHECK(NOT NULL));
+CREATE TABLE IF NOT EXISTS ServiceMaintenance(SMID SERIAL PRIMARY KEY, FName varchar(50), LName varchar(50), Bike varchar(50), Service varchar(100), WorkedBy INTEGER REFERENCES Worker(WID), WorkStatus varchar(15), Notes VARCHAR(180), STime TIMESTAMP, ETime TIMESTAMP, TID INTEGER REFERENCES Transactions(TID));
+CREATE TABLE IF NOT EXISTS PunchCard(PCID serial primary key, stampType varchar(5), stamp timestamp, WID INTEGER REFERENCES Worker(WID));
+CREATE TABLE IF NOT EXISTS Plans(PID INTEGER primary key, name varchar(15), amount integer);
 
 /*
 Create User
  */
-INSERT INTO USERS( FName, LName, password, PNumber, Email) VALUES ('F', 'L', crypt('password', gen_salt('bf')), '5555555555', 'e@email.com') RETURNING uid;
-
+INSERT INTO USERS( FName, LName, password, PNumber, Email, Confirmation) VALUES ('I', 'Couver', crypt('password', gen_salt('bf')), '5555555555', 'i@ece.com', true) RETURNING uid;
+SELECT * FROM USERS WHERE Email= 'e@r.com';
 /*
 Create Admin, Create Worker, Client
  */
-INSERT INTO Admin (UID )VALUES ((Select  UID FROM Users where Email = 'email@ree.com') );
-INSERT INTO Client (UID )VALUES ((Select  UID FROM Users where Email = 'email@ree.com') );
-INSERT INTO Worker (UID )VALUES ((Select  UID FROM Users where Email = 'email@ree.com') );
+INSERT INTO Admin (UID )VALUES ((Select  UID FROM Users where Email = 'e@mail.com') );
+Select  UID FROM Users where Email = 'e@f.com';
+INSERT INTO Client (UID )VALUES ((Select  UID FROM Users where Email = 'fgfgf') ) RETURNING CID;
+SELECT * FROM Client NATURAL INNER JOIN Users WHERE Email = 'e@r.com';
+INSERT INTO Worker (UID )VALUES ((Select  UID FROM Users where Email = 'e@mail.com') );
 
 /*
 Logins:
  */
 
-SELECT email = 'email@ree.com'  AND password = crypt('UsersPassword', password) from Users;
+SELECT UID from Users where email = 'e@r.com'  AND password = crypt('passw', password) ;
+
+/*
+Add Bike to system
+ */
+
+INSERT INTO Bike(lp, rfid, bikestatus, brand, model) VALUES ('1236', '123456', 'Available', 'OFO', 'HISTANDARD');
 
 /*
 Available Bikes
@@ -50,14 +57,14 @@ Rent Bike in Mobile App
 /*
     Reserve Bike, Revert If Transaction Fails
  */
-UPDATE bike SET status = 'Reserved' WHERE BID in (SELECT bid from bike where status = 'Available' limit 1) returning bid;
+UPDATE bike SET bikestatus = 'RESERVED' WHERE BID in (SELECT bid from bike where bikestatus = 'Available' limit 1) returning bid;
 
 /*
     Save Successful Transaction and create rental object
  */
-INSERT INTO transactions(stamp, token, cid, status) VALUES (now(), 'token', 1, 'Completed') returning TID;
-INSERT INTO rental(stime, client, dueDate) VALUES (now(), CID,now() +8 ) RETURNING RID;
-INSERT INTO RentLink(RID, TID) VALUES (RID, TID);
+INSERT INTO transactions(stamp, token, cid, status, Cost) VALUES (now(), 'token', 11, 'Completed', (SELECT amount from plans WHERE PID = 1)) returning TID;
+INSERT INTO rental(stime, client, dueDate) VALUES (now(), 11,current_date +8 ) RETURNING RID;
+INSERT INTO RentLink(RID, TID) VALUES (3, 11);
 
 /*
    Update Rental Upon Dispatch
@@ -67,12 +74,12 @@ INSERT INTO RentLink(RID, TID) VALUES (RID, TID);
   /*
   Find Rental to be Updated
    */
-  UPDATE Rental set DispatchedBy = 'WorkerOnTurn' where RID = 'ridGiven';
+  UPDATE Rental set DispatchedBy = 1 where RID = 3;
 
 /*
 Find bike scanned
  */
-SELECT BID, Status FROM bike WHERE RFID =  'RFIDScanned' or LP = 'LPGiven';
+SELECT BID, Bikestatus FROM bike WHERE RFID =  '1111111' or LP = '1236';
 
 /*
 If Status = 'Available' reserve bike and release 1 reserved bike
@@ -82,31 +89,33 @@ UPDATE Bike set Status = 'Available' where bid = (SELECT BID from bike where Sta
  /*
  If Status = Reserved update to rented
   */
- UPDATE Bike SET Status = 'Rented' where BID = 'bidGiven';
+ UPDATE Bike SET Status = 'Rented' where BID = 3;
+
 
 /*
 Update Rental by inserting final bike dispatched
  */
-UPDATE rental SET BID = 'bidGiven' where RID = 'ridGiven';
+UPDATE rental SET BID = 3 where RID = 3;
 
 
 /*
    Update Upon Rental Return
  */
 
- UPDATE Rental set ETime = now(), RecivedBy = 'WorkerOnStation' where RID = (SELECT rid from bike inner join Rental ON Bike.BID = Rental.BID AND RFID = 'rfidScanned' and RecivedBy isnull );
+ UPDATE Rental set ETime = now(), ReceivedBy = 1 where RID = (SELECT rid from bike inner join Rental ON Bike.BID = Rental.BID AND RFID = '111111111' and rental.ReceivedBy isnull );
+ UPDATE BIKE SET bikestatus = 'AVAILABLE' WHERE bID = (SELECT BID FROM Bike WHERE RFID = '111111111');
 
 /*
 Create ServiceMaintenance
  */
 
- INSERT INTO ServiceMaintenance(fname, lname, bike, service, price, workstatus, notes, stime ) VALUES ('fNAme', 'lName', 'bike', 'service', 5, 'StandBy', 'notes', now());
+ INSERT INTO ServiceMaintenance(fname, lname, bike, service, workstatus, notes, stime ) VALUES ('fNAme', 'lName', 'bike', 'service', 'StandBy', 'notes', now());
 
 /*
 Checkout ServiceMaintenance
  */
 
- UPDATE ServiceMaintenance set WorkedBy = 'Worker on turn', WorkStatus='Done', ETime = now() where SMID ='smidGiven'; 
+ UPDATE ServiceMaintenance set WorkedBy = 1, WorkStatus='Done', ETime = now() where SMID = 1;
 
 /*
 Decomission Request
@@ -124,16 +133,42 @@ Update Request
 Create new Maintainance Request
  */
 
- INSERT INTO Maintenance(starttime, status, notes, bid, requestedby) VALUES (now(), 'Pending', 'Notes', 'bidGiven', 'requestor');
+ INSERT INTO Maintenance(starttime, status, bid, requestedby) VALUES (now(), 'Pending',  'bidGiven', 'requestor');
 
 /*
 Update MaintenanceRequest
  */
 
- update Maintenance set servicedby = 'Server', EndTime= now() where MID = 'midGiven';
+ update Maintenance set servicedby = 'Server', EndTime= now(), Service = 'Services Done', Notes = 'Additional Details' , Status = 'Finished' where MID = 'midGiven' and Status ='Pending' ;
 
 /*
 PunchCard
  */
 
- insert into PunchCard(stampType, STAMP) VALUES ('In / Out ', now();)
+ insert into PunchCard(stampType, STAMP, WID) VALUES ('In / Out ', now(), 'Worker_ID');
+
+/*
+Financial Report
+ */
+
+/*
+Get Rentals in the Last 7 Days
+ */
+ select count(*) from Rental where current_date - STime::date < 7;
+
+/*
+Get Revenue for the last 7 days
+ */
+ select sum(cost) from Transactions where Status !='Failed' and current_date - stamp :: date <7;
+
+/*
+Cash Late Fee
+ */
+
+ SELECT  ceil((DATE_PART('day', current_date - (SELECT dueDate from Rental where RID = 2))/7) )* (SELECT amount from plans WHERE PID = 2);
+
+SELECT  current_date - (SELECT dueDate from Rental where RID = 2)
+
+select * from Maintenance
+
+select * from Plans
