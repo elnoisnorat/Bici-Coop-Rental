8cd1c60f9126f0b4102a9d64c134cabab37542c4
JAgosto95 <juan.agosto7@upr.edu>
2019-04-16 18:40:53 -0400

    Update update password

---
+++ config/validation.py
@@ -0,0 +1,108 @@
+from flask import request, jsonify
+from functools import wraps
+from flask_login import current_user
+from dao.bicycle import BicycleDAO
+
+
+def isWorker(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if current_user.role != "Worker":
+            return jsonify(Error="Not a worker."), 403
+
+        return f(*args, **kwargs)
+    return decorated
+
+def isAdmin(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if current_user.role != "Admin":
+            return jsonify(Error="Not an admin."), 403
+        return f(*args, **kwargs)
+    return decorated
+
+def isClient(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if current_user.role != "Client":
+            return jsonify(Error="Not a client."), 403
+        return f(*args, **kwargs)
+    return decorated
+
+def isWorkerOrAdmin(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if current_user.role != "Worker" and current_user.role != "Admin":
+            return jsonify(Error="Not a valid user."), 403
+        return f(*args, **kwargs)
+    return decorated
+
+def isWorkerOrClient(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if current_user.role != "Worker" and current_user.role != "Client":
+            return jsonify(Error="Not a valid user."), 403
+        return f(*args, **kwargs)
+    return decorated
+
+def hasRole(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if current_user.role != "Worker" and current_user.role != "Admin" and current_user.role != "Client":
+            return jsonify(Error="Not a valid user."), 403
+        return f(*args, **kwargs)
+    return decorated
+
+def isDecomissioned(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        bDAO = BicycleDAO()
+        bStatus = bDAO.getStatusByID(args[1])
+        if bStatus == 'DECOMISSIONED':
+            return jsonify(Error="This bicycle is decomissioned")
+        return f(*args, **kwargs)
+    return decorated
+
+def validPassword(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        password = request.json['password']
+        size = len(password)
+        if size >= 8 \
+                and any(a.islower() for a in password) \
+                and any(a.isupper() for a in password) \
+                and any(a.isnumeric() for a in password):
+                pass
+        else:
+            return jsonify(Error="Malformed password."), 400
+        return f(*args, **kwargs)
+    return decorated
+
+def validUpdatePassword(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        try:
+            password_list = []
+            oldPassword = request.json['oldPassword']
+            newPassword = request.json['newPassword']
+            confirmPassword = request.json['confirmPassword']
+            if oldPassword != newPassword and newPassword == confirmPassword:
+                password_list.append(oldPassword)
+                password_list.append(newPassword)
+                for password in password_list:
+                    size = len(password)
+                    if size >= 8 \
+                            and any(a.islower() for a in password) \
+                            and any(a.isupper() for a in password) \
+                            and any(a.isnumeric() for a in password):
+                        pass
+                    else:
+                        return jsonify(Error="Malformed password."), 400
+            else:
+                return jsonify(Error="Malformed update request."), 400
+        except:
+            return jsonify(Error="Malformed update request."), 400
+
+        return f(*args, **kwargs)
+
+    return decorated
