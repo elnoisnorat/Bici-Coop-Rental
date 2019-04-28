06335ca75048f93834ccfa55866c5e0ecdd4404f
JAgosto95 <juan.agosto7@upr.edu>
2019-04-28 18:59:13 -0400

    Edit sent emails

--- handler/newEmail.py
+++ handler/newEmail.py
@@ -44,9 +44,10 @@ class EmailHandler():
         content = str(token)
         link = AWS_LINK + "/forgotPassword?value=" + str(content[2:len(content)-1])
         msg = Message('Password Recovery Confirmation', recipients=[email])
-        body = '''A request for a new password has been made for this account. " \
-                   "Click <a href="''' + link + '''"> here</a> to receive a new password. " \
-                   "If you did not issue this request, please ignore this message.'''
+        body = '''A request for a new password has been made for this account. 
+        Click <a href="''' + link + '''"> here</a> to receive a new password. 
+        If you did not issue this request, please ignore this message.
+        '''
         msg.html = body
 
         try:
