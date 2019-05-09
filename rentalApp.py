070de8d3751897eefc19409cb33ed21114d4c8f0
JAgosto95 <juan.agosto7@upr.edu>
2019-05-08 20:31:44 -0400

    Extensive validation Updating error messages

--- rentalApp.py
+++ rentalApp.py
@@ -900,6 +900,5 @@ def logout():
     return jsonify('You are now logged out')
 
 if __name__ == '__main__':
-    app.run(ssl_context=('cert.pem', 'key.pem'))
-
+    app.run()
     #debug=True)#, ssl_context=('cert.pem', 'key.pem'))
